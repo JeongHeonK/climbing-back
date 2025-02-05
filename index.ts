790f253c7a0e97ae30138f2e36747644c54d9ffa
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-05 22:10:46 +0900

    feat: lint 적용 수정
    - airbnb 적용 시 Import 사용과 jsx 미사용 충돌 발생
    - 그냥 가벼운 정의로 변경

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import "dotenv/config";
-import mongoose from "mongoose";
-import { PORT, URL } from "./constant";
-import { loginRoute, signupRoute, gatheringRoute } from "./routes";
+import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import { PORT, URL } from './constant';
+import { loginRoute, signupRoute, gatheringRoute } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/login", loginRoute);
-app.use("/signup", signupRoute);
-app.use("/gathering", gatheringRoute);
+app.use('/login', loginRoute);
+app.use('/signup', signupRoute);
+app.use('/gathering', gatheringRoute);
 
-app.listen(PORT, () => console.log("Server started"));
-mongoose.connect(URL).then(() => console.log("connect db"));
+app.listen(PORT, () => console.log('Server started'));
+mongoose.connect(URL).then(() => console.log('connect db'));
