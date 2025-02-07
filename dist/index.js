2364f34edbba13d9c0f6801e4116ff78bb646ec4
JeongHeon <kdrlawjdgjs@gmail.com>
2025-02-07 19:22:04 +0900

    chore: js 변환

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,17 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-require('dotenv/config');
-const mongoose_1 = __importDefault(require('mongoose'));
-const User_1 = __importDefault(require('./model/User'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const constant_1 = require("./constant");
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
-const url = process.env.MONGO_DB_URL;
-app.get('/', (req, res) => {
-  res.send('docker랑 병행중');
-});
-app.get('/user/:email', async (req, res) => {
-  const { email } = req.params;
-  const existingUser = User_1.default.find({ email });
-  res.send(!!existingUser);
-});
-app.listen(8080, () => console.log('Server started'));
-mongoose_1.default.connect(url).then(() => console.log('connect db'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use('/login', routes_1.loginRoute);
+app.use('/signup', routes_1.signupRoute);
+app.use('/gathering', routes_1.gatheringRoute);
+app.listen(constant_1.PORT, () => console.log('Server started'));
+//# sourceMappingURL=index.js.map
--- dist/model/User.js
+++ dist/model/User.js
@@ -1,21 +1,22 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const mongoose_1 = __importDefault(require('mongoose'));
-const UserSchema = new mongoose_1.default.Schema(
-  {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const UserSchema = new mongoose_1.default.Schema({
     email: {
-      type: String,
+        type: String,
+        unique: true,
+        required: true,
     },
     password: {
-      type: String,
+        type: String,
+        required: true,
+        maxLength: 12,
+        minLength: 8,
     },
-  },
-  { timestamps: true },
-);
+}, { timestamps: true });
 const User = mongoose_1.default.model('User', UserSchema);
 exports.default = User;
+//# sourceMappingURL=User.js.map
